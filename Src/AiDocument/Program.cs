95d5919f79a85a19ee512935aeea0469f1ab16d9
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-12-03 01:06:00 +0000

    feat - add initial solution and project files for tool

---
+++ Src/AiDocument/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AiDocument;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Tests/AIDocument.Tests/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace AIDocument.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() { }
+}
